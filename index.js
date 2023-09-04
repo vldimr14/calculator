0681f3c2fde6363f978cf1b6a2e07b9aeeed78a1
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2023-09-04 11:41:55 +0200

    complete basic math logic

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
+const display = document.querySelector('.display');
+const digits = document.querySelectorAll('.digit'); 
+const clearBtn = document.querySelector('.clear');
+const dotBtn = document.querySelector('.dot');
+const operators = document.querySelectorAll('.operator');
+const equalsBtn = document.querySelector('.equals');
+
+
+
+let displayValue = 0;
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+let divisionByZeroMessage = 'You can\'t divide by 0!';
+
+clearBtn.addEventListener('click', () => {
+    clearDisplay();
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = '';
+});
+
+digits.forEach((digit) => digit.addEventListener('click', (e) => {
+    updateDisplay(e.target.textContent);
+}));
+
+dotBtn.addEventListener('click', (e) => {
+    updateDisplay(e.target.textContent);
+});
+
+operators.forEach((oper) => oper.addEventListener('click', (e) => {
+    if (display.textContent !== '') {
+        firstNumber = +display.textContent;
+        operator = oper.textContent;
+
+        clearDisplay();
+    }
+}));
+
+equalsBtn.addEventListener('click', () => {
+    if (display.textContent !== '') {
+        secondNumber = +display.textContent;
+        operate(firstNumber, secondNumber, operator);
+    }
+});
+
+function clearDisplay() {
+    displayValue = 0;
+    display.textContent = '';
+}
+
+function updateDisplay(value) {
+    if (display.textContent === divisionByZeroMessage) {
+        return;
+    }
+
+    let number = Array.from(display.textContent);
+
+    if (number.includes('.') && value === '.') {
+        return;
+    }
+
+    displayValue = value;
+    display.textContent += displayValue;
+}
+
 function operate(firstNumber, secondNumber, operator) {
     let result;
 
@@ -11,7 +77,11 @@ function operate(firstNumber, secondNumber, operator) {
         result = divide(firstNumber, secondNumber);
     }
 
-    return result;
+    if (!isNaN(result)) {
+        display.textContent = result.toFixed(2);   
+    } else {
+        display.textContent = result;
+    }
 }
 
 function add(firstNumber, secondNumber) {
@@ -27,5 +97,9 @@ function multiply(firstNumber, secondNumber) {
 }
 
 function divide(firstNumber, secondNumber) {
+    if (secondNumber === 0) {
+        return divisionByZeroMessage;
+    }
+    
     return firstNumber / secondNumber;
 }
